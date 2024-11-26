ebb4ae0644e6cc3d9bbe17fa8f44d93a69351c01
weirdlylikeable <ayan2452007@gmail.com>
2024-11-26 19:34:14 +0500

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,6 @@
+number=int(input("Enter a number"))
+
+if number%2==0:
+    print("This number is even.")
+else:
+    print("This number is odd.")
---
+++ main3part4.py
@@ -0,0 +1,8 @@
+import datetime
+
+current_date=datetime.datetime.now()
+print(current_date)
+
+import calendar
+current_year_calender=calendar.calendar(2024)
+print(current_year_calender)

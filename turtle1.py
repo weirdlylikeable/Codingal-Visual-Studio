52b859716e56ae4a4fbc1403ecb20922d2b0d316
weirdlylikeable <ayan2452007@gmail.com>
2024-12-10 19:45:20 +0500

    Add files via upload

---
+++ turtle1.py
@@ -0,0 +1,20 @@
+import turtle
+pointer=turtle.Turtle()
+
+pointer.forward(100)
+pointer.left(60)
+pointer.forward(100)
+pointer.left(60)
+pointer.forward(100)
+pointer.left(60)
+pointer.forward(100)
+pointer.left(60)
+pointer.forward(100)
+pointer.left(60)
+pointer.forward(100)
+
+
+
+
+
+turtle.done()

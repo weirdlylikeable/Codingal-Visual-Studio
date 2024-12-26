e0fbde567d49f53861043da354b42ec90ffd6888
weirdlylikeable <ayan2452007@gmail.com>
2024-12-26 10:56:33 +0500

    Add files via upload

---
+++ tupletolist.py
@@ -0,0 +1,8 @@
+def tuple_to_list(input_tuple):
+    return list(input_tuple)
+
+my_tuple = (1, 2, 3, 4, 5)
+converted_list = tuple_to_list(my_tuple)
+
+print("Original Tuple:", my_tuple)
+print("Converted List:", converted_list)
